9f9c840b7a6978bad8cd757ec4110f5e53563d88
DylanReedy <dylan.p.reedy@gmail.com>
2018-01-29 22:07:02 -0800

    updated interface javadoc info

--- src/main/java/com/rapidprototyping/manbot/model/command/CommandParser.java
+++ src/main/java/com/rapidprototyping/manbot/model/command/CommandParser.java
@@ -1,8 +1,17 @@
 package com.rapidprototyping.manbot.model.command;
 
+import java.util.ArrayList;
+
 public interface CommandParser
 {
-    boolean isCommandCandidate(String input);
-
-    Command parseCommand(String input);
+        /**
+         * @param input Any non-null string
+         * @return Returns a list of whitespace separated tokens.
+         */
+        boolean isCommandCandidate(String input);
+        /**
+         * @param input Any non-null string
+         * @return Returns false if it finds no command indicator, true otherwise
+         */
+        ArrayList<String> tokenize(String input);
 }
