a14798a83f2e96abbacecab6a609982246ce239e
DylanReedy <dylan.p.reedy@gmail.com>
2018-02-09 00:21:12 -0800

    inputtexttokenizer and its simple implementation had no code on initial commit

--- src/main/java/com/rapidprototyping/manbot/controller/InputTextTokenizer.java
+++ src/main/java/com/rapidprototyping/manbot/controller/InputTextTokenizer.java
@@ -1,4 +1,16 @@
 package com.rapidprototyping.manbot.controller;
 
+import java.util.ArrayList;
+
 public interface InputTextTokenizer {
+    /**
+     * @param input Any non-null string
+     * @return Returns a list of whitespace separated tokens.
+     */
+    boolean isCommandCandidate(String input);
+    /**
+     * @param input Any non-null string
+     * @return Returns false if it finds no command indicator, true otherwise
+     */
+    ArrayList<String> tokenize(String input);
 }
